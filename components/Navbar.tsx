73812b84131d9ad51f7be642cfb477d265b63a43
anton <azvontsov@gmail.com>
2022-07-23 14:22:01 -0400

    adds logo to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,22 @@ import {AiOutlineLogout} from "react-icons/ai"
 import {BiSearch} from "react-icons/bi"
 import {IoMdAdd} from "react-icons/io"
 
+import Logo from '../utils/tictac-logo.png'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+      <div className='w-[100px]'>
+        <Image 
+        className='cursor-pointer'
+        src={Logo}
+        alt='tictac'
+        layout='responsive'
+        />
+      </div>
+      </Link>
+    </div>
   )
 }
 
