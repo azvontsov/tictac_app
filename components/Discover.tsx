fe2269aedeb7a016d92507f9c9020df3f4425e62
anton <azvontsov@gmail.com>
2022-07-23 16:01:55 -0400

    adds Discover component

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -10,6 +10,20 @@ const Discover = () => {
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
         <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>Popular Topics</p>
+    <div className='flex gap-3 flex-wrap'>
+        {topics.map((item) => (
+            <Link href={`/?topic=${item.name} `}key={item.name}>
+                <div>
+                    <span className='font-bold text-2xl'>
+                        {item.icon}
+                    </span>
+                    <span className='font-medium text-md hidden xl:block capitalize'>
+                        {item.name}
+                    </span>
+                </div>
+            </Link>
+        ))}
+    </div>
     </div>
   )
 }
