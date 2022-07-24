8d7e4c676c8deb2334965f5e498970f06f4bad76
anton <azvontsov@gmail.com>
2022-07-24 15:34:39 -0400

    adds picto to video container

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -13,7 +13,10 @@ interface IProps {
 
 
 const VideoCard: NextPage<IProps> = ({post}) => {
-console.log("caption:", post.postedBy)
+
+    const [isHover, setIsHover] = useState(false);
+    const [playing, setPlaying] = useState(false);
+    const [isVideoMuted, setIsVideoMuted] = useState(false);
 
   return (
     <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
@@ -46,13 +49,38 @@ console.log("caption:", post.postedBy)
         </div>
       </div>
       <div className='lg:ml-20 flex gap-4 relative'>
-        <div className='rounded-3x1'>
+        <div 
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className='rounded-3x1'>
           <Link href='/'>
             <video 
             loop
             className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
             src={post.video.asset.url}></video>
           </Link>
+          {isHover && (
+            <div>
+                {playing ? (
+                  <button>
+                    <BsFillPauseFill className='text-black text-2xl lg:text-4xl'/>
+                  </button>
+                ) : (
+                  <button>
+                    <BsFillPlayFill className='text-black text-2xl lg:text-4xl'/>
+                  </button>
+                )}
+                {isVideoMuted ? (
+                  <button>
+                    <HiVolumeOff className='text-black text-2xl lg:text-4xl'/>
+                  </button>
+                ) : (
+                  <button>
+                    <HiVolumeUp className='text-black text-2xl lg:text-4xl'/>
+                  </button>
+                )}
+            </div>
+          )}
         </div>
       </div>
     </div>
