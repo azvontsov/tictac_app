419b7c179236ffbb59708403296d9cb2ba79b277
anton <azvontsov@gmail.com>
2022-07-24 13:59:32 -0400

    create api localhost:3000

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
+import axios from 'axios';
 
+// interface IProps {
+//   videos:
+// }
 
-const Home: NextPage = () => {
+const Home: NextPage = ({videos}) => {
+  console.log(videos)
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
@@ -9,6 +14,17 @@ const Home: NextPage = () => {
     </h1>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+
+  return{
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
