e32b9aee392c17bad4d3d7a95d99b1803e5e1274
anton <azvontsov@gmail.com>
2022-07-25 15:40:50 -0400

    adds search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -6,7 +6,7 @@ import {MdLogout} from "react-icons/md";
 import {BiSearch} from "react-icons/bi";
 import {IoMdAdd} from "react-icons/io";
 import { GoogleLogin, googleLogout  } from '@react-oauth/google';
-
+import { IUser } from '../types';
 import Logo from '../utils/tictac-logo.png';
 import { createOrGetUser } from '../utils';
 import useAuthStore from '../store/authStore';
@@ -14,6 +14,29 @@ import useAuthStore from '../store/authStore';
 const Navbar = () => {
 
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState('');
+  const [user, setUser] = useState<IUser | null>();
+  const router = useRouter();
+
+
+
+
+
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
+
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    
+    if(searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
+
+
+
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -27,7 +50,26 @@ const Navbar = () => {
         />
       </div>
       </Link>
-      <div>SEARCH</div>
+      <div className='relative hidden md:block'>
+        <form
+          onSubmit={handleSearch}
+          className='absolute md:static top-10 -left-20 bg-white'
+        >
+          <input
+          type='text'
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-[#ffd000] w-[300px] md:w-[350px] rounded-full  md:top-0'
+            placeholder='Search accounts and videos'
+          />
+          <button
+            onClick={handleSearch}
+            className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>
         {userProfile ? (
           <div className='flex gap-5 md:gap-10'>
@@ -38,7 +80,7 @@ const Navbar = () => {
               </button>
             </Link>
             {userProfile.image && (
-              <Link href="/">
+              <Link href="href={`/profile/${user._id}`}">
               <>
               <Image
                 width={40}
