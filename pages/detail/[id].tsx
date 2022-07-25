30ba12aa7c6e5ebfcb46809eacb88c2d8a80f2fe
anton <azvontsov@gmail.com>
2022-07-25 12:54:38 -0400

    adds likes

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,8 +9,9 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
 import { Video } from '../../types';
-
-
+import useAuthStore from '../../store/authStore';
+import Comments from '../../components/Comments';
+import LikeButton from '../../components/LikeButton';
 
 interface IProps {
   postDetails: Video;
@@ -25,6 +26,7 @@ const Detail = ({ postDetails }: IProps) => {
 
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  const { userProfile }: any = useAuthStore();
 
   const onVideoClick = () => {
     if (isPlaying) {
@@ -43,6 +45,36 @@ const Detail = ({ postDetails }: IProps) => {
   }, [post, isVideoMuted]);
 
 
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      const res = await axios.put(`${BASE_URL}/api/like`, {
+        userId: userProfile._id,
+        postId: post._id,
+        like
+      });
+      setPost({ ...post, likes: res.data.likes });
+    }
+  };
+
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (userProfile) {
+      if (comment) {
+        setIsPostingComment(true);
+        const res = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+          userId: userProfile._id,
+          comment,
+        });
+
+        setPost({ ...post, comments: res.data.comments });
+        setComment('');
+        setIsPostingComment(false);
+      }
+    }
+  };
+
+
 
   if(!post) return null;
 
@@ -56,6 +88,9 @@ const Detail = ({ postDetails }: IProps) => {
             <MdOutlineCancel className='text-white text-[35px] hover:opacity-90' />
           </p>
         </div>
+
+         {/* PLAY VIDEO */}
+
         <div className='lg:h-[100vh] h-[60vh]'>
           <video
             ref={videoRef}
@@ -75,6 +110,9 @@ const Detail = ({ postDetails }: IProps) => {
           )}
         </div>
         </div>
+
+            {/* MUTE VIDEO */}
+        
         <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10  cursor-pointer'>
           {isVideoMuted ? (
             <button onClick={() => setIsVideoMuted(false)}>
@@ -85,7 +123,54 @@ const Detail = ({ postDetails }: IProps) => {
               <HiVolumeUp className='text-white text-3xl lg:text-4xl' />
             </button>
           )}
+        </div>
+      </div>
+
+      {/* COMMENT BAR */}
+
+      <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+        <div className='lg:mt-20 mt-10'>
+          <Link href={`/profile/${post.postedBy._id}`}>
+            <div className='flex gap-3 p-2 mb-4 bg-white w-full pl-10 cursor-pointer '>
+              <Image
+                width={60}
+                height={60}
+                alt='user-profile'
+                className='rounded-full'
+                src={post.postedBy.image}
+              />
+              <div className='flex flex-col gap-2'>
+                <p className='flex gap-2 md:text-sm font-bold text-primary'>
+                  {post.postedBy.userName.replace(/\s+/g, '')}{' '}
+                  <GoVerified className='text-blue-400 text-sm' />
+                </p>
+                <p className='text-md'> {post.postedBy.userName}</p>
+              </div>
             </div>
+          </Link>
+
+          {/* CAPTIONS */}
+          <div className='px-10'>
+            <p className=' text-md text-gray-600'>{post.caption}</p>
+          </div>
+
+          {/* LIKES/ DISLIKES */}
+          <div className='mt-10 px-10'>
+            {userProfile && <LikeButton
+              likes={post.likes}
+              flex='flex'
+              handleLike={() => handleLike(true)}
+              handleDislike={() => handleLike(false)}
+            />}
+          </div>
+          <Comments
+            // comment={comment}
+            // setComment={setComment}
+            // addComment={addComment}
+            // comments={post.comments}
+            // isPostingComment={isPostingComment}
+          />
+        </div>
       </div>
     </div>
   )
